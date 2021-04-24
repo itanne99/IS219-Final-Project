5d555a947250d252701215a9e4a250d5f6f98279
Itanne99 <idovader@gmail.com>
2021-04-24 19:18:59 -0400

    Implemented table view

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api')
-var formRouter = require('./routes/form')
+var apiRouter = require('./routes/api');
+var formRouter = require('./routes/form');
+var tableRouter = require('./routes/table');
 
 var app = express();
 
@@ -30,12 +31,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/form', formRouter);
+app.use('/table', tableRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
