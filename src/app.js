7ac7a865fbc89fa7f5a75a160543c83a204f5b93
Ido Tanne <idovader@gmail.com>
2021-04-29 15:10:48 -0400

    Feature: Added passport.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 const webpack = require('webpack');
 const config = require('../config/webpack.dev');
 
+/*Webpack Config*/
 const compile = webpack(config);
 const webpackHotMiddleware = require('webpack-hot-middleware')(compile);
 const webpackDevMiddleware = require('webpack-dev-middleware')(compile, config.devServer);
+require("dotenv").config();
 
+/*Passport Config*/
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false,
+};
+
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    },
+);
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
+
+/*Routers*/
 var indexRouter = require('./routes');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 var formRouter = require('./routes/form');
 var tableRouter = require('./routes/table');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -29,11 +69,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/form', formRouter);
 app.use('/table', tableRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
