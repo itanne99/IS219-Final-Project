65cfb4443e1ce069f4e50a2d1ef5ad6421ae86f4
Ido Tanne <idovader@gmail.com>
2021-04-29 19:50:04 -0400

    Task: Added pug files to webpack

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './static/js/jquery';
 import './static/js/bootstrap.bundle';
 import './static/css/bootstrap.css';
 import './static/images/img1.jpg';
+import './views/viewFiles';
 
 require('babel-runtime/regenerator');
 require('webpack-hot-middleware/client');
---
+++ src/views/viewFiles.js
@@ -0,0 +1,6 @@
+import './layout.pug';
+import './error.pug';
+import './index.pug';
+import './tableView.pug';
+import './newUser.pug';
+import './editUser.pug';
