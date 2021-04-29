86758168b6883f54b5b0dcc5d2f3252c1cdf4628
Ido Tanne <idovader@gmail.com>
2021-04-29 13:28:17 -0400

    Feature: Added Front-end Login/Registration portal

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,20 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function (req, res, next) {
+  res.render('userPortal', { title: 'Login Portal', mode: 'login' });
+});
+
+router.post('/login', function (req, res, next) {
+  // Login user
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('userPortal', { title: 'Register Portal', mode: 'register' });
+});
+
+router.post('/register', function (req, res, next) {
+  // Register user
+});
+
 module.exports = router;
