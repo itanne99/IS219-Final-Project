7ca4425fa67c0690cc858223ce24e2c3353918e8
Ido Tanne <idovader@gmail.com>
2021-05-01 23:31:36 -0400

    Feature: made a simple json that requires a user login

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,25 @@
-var express = require('express');
+const express = require('express');
+const authenticationToken = require('./auth').authenticationToken;
+const router = express.Router();
 
-var router = express.Router();
+const posts = [
+  {
+    username: 'Ido',
+    title: 'Post 1'
+  },
+  {
+    username: 'Jane',
+    title: 'Post 2'
+  },
+]
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/posts', authenticationToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name ));
 });
 
 module.exports = router;
