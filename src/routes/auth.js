4109d53475ee8397e34f3f25c788f151a6815877
Ido Tanne <idovader@gmail.com>
2021-05-01 23:32:36 -0400

    Feature: Added Login system & Token Refresh system

---
+++ src/routes/auth.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+let refreshTokens = [];
+
+router.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.status(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        const accessToken = generateAuthToken({name: user.name});
+        res.json({accessToken:accessToken});
+    });
+});
+
+router.post('/login', (req, res) => {
+    // Authenticate User
+    const username = req.body.username;
+    const user = { name: username};
+
+    const accessToken = generateAuthToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+    res.json({accessToken: accessToken, refreshToken: refreshToken});
+});
+
+function authenticationToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+function generateAuthToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m'});
+}
+
+module.exports = { router, authenticationToken};
